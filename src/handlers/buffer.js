73f0ee1cac82726e8df5f44a15287652a2505c8b
Jason <jason.daurus@gmail.com>
2016-09-05 15:38:07 +0800

    decompress response in handlers

--- src/handlers/buffer.js
+++ src/handlers/buffer.js
@@ -1,10 +1,33 @@
+import zlib from 'zlib'
+import stream from 'stream'
 import waitFor from 'event-to-promise'
 
 export default async function(res) {
   const buffers = []
-  res.on('data', chunk => {
+  const encoding = res.headers['content-encoding']
+
+  let body = res.pipe(new stream.PassThrough)
+
+  if (encoding == 'gzip' || encoding == 'x-gzip') {
+
+    body = body.pipe(zlib.createGunzip())
+
+  } else if (encoding == 'deflate' || encoding == 'x-deflate') {
+
+    const chunk = await waitFor(res.pipe(new stream.PassThrough), 'data')
+
+    if ((chunk[0] & 0x0F) === 0x08) {
+      body = body.pipe(zlib.createInflate())
+    } else {
+      body = body.pipe(zlib.createInflateRaw())
+    }
+  }
+
+  body.on('data', chunk => {
     buffers.push(chunk)
   })
-  await waitFor(res, 'end', true)
+
+  await waitFor(body, 'end')
+
   return Buffer.concat(buffers)
 }
